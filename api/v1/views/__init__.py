af0104c2ded0fe6122d8e2f8483d594d93001016
Fatima20027 <fatima.hailou@icloud.com>
2024-08-03 19:07:10 +0100

    task 3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,6 @@ Initialization file for the api.v1.views package.
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
