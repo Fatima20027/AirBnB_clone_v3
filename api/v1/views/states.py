101bcea5755c714bc98ff29a7b5ed8b1492ba6db
Fatima20027 <fatima.hailou@icloud.com>
2024-08-04 21:30:11 +0100

    some update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """
@@ -15,6 +16,7 @@ def get_states():
     states = storage.all(State).values()
     states_list = [state.to_dict() for state in states]
     return jsonify(states_list)
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
@@ -26,7 +28,10 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """
     Deletes a State object.
@@ -37,6 +42,7 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -52,6 +58,7 @@ def create_state():
     new_state = State(**request_data)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
