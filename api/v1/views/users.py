4d5422774d6eceb4e42d699805522cb0d35f78aa
Fatima20027 <fatima.hailou@icloud.com>
2024-08-05 18:24:07 +0100

    task 9

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,8 +8,7 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/users',
-                 methods=['GET'], strict_slashes=False)
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 def all_users():
     """
     Retrieves the list of all User objects.
@@ -19,8 +18,7 @@ def all_users():
     return jsonify(users_list)
 
 
-@app_views.route('/users/<user_id>',
-                 methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def id_user(user_id):
     """
     Retrieves a User object.
@@ -31,9 +29,7 @@ def id_user(user_id):
     return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<user_id>',
-                 methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """
     Deletes a User object.
@@ -56,7 +52,6 @@ def create_user():
     request_data = request.get_json()
     if 'email' not in request_data:
         abort(400, 'Missing email')
-
     if 'password' not in request_data:
         abort(400, 'Missing password')
 
@@ -65,8 +60,7 @@ def create_user():
     return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>',
-                 methods=['PUT'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """
     Updates a User object.
@@ -78,7 +72,7 @@ def update_user(user_id):
         abort(400, 'Not a JSON')
     request_data = request.get_json()
     for key, value in request_data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
             setattr(user, key, value)
     user.save()
     return jsonify(user.to_dict()), 200
