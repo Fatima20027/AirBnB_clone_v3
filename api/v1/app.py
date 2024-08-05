0346a23c77ee5b4a991e2dd37bd1db7fb802337e
Fatima20027 <fatima.hailou@icloud.com>
2024-08-05 19:41:05 +0100

    task cities

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,13 @@ api
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
